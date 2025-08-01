bad8ecbd6dcd0eb141600f6f28d5fd5131a68a23
Ai22anas <2nas.mohammed@gmail.com>
2025-07-31 23:26:34 -0700

    feat: add frontend App component

---
+++ packages/frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Champions Arena</h1>
+      <p>Welcome to the Champions Arena football tournament platform!</p>
+    </div>
+  );
+}
+
+export default App;
